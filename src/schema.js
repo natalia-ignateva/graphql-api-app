797c1875930b4986cdfab084df81bfd8da6fa12d
Natalia Ignateva <ignatyevanat@gmail.com>
2021-07-01 16:00:08 +0200

    Create schema with nested types

--- src/schema.js
+++ src/schema.js
@@ -1,18 +1,89 @@
 const graphql = require('graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql;
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+} = graphql;
 
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
-        },
+  query: queryType,
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    post: {
+      type: postType,
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (source, { id }) => {
+        return posts[id];
       },
     },
-  }),
+    posts: {
+      type: new GraphQLList(postType),
+      resolve: () => {
+        return posts;
+      },
+    },
+  },
 });
+
+const postType = new GraphQLObjectType({
+  name: 'Post',
+  fields: {
+    title: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    author: {
+      type: authorType,
+      resolve: (source, params) => {
+        return authors[source.author];
+      },
+    },
+  },
+});
+
+const authorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: {
+    name: {
+      type: GraphQLString,
+    },
+    age: {
+      type: GraphQLInt,
+    },
+  },
+});
+
+const posts = [
+  {
+    title: 'First post',
+    description: 'Content of the first post',
+    author: 'Daisy',
+  },
+  {
+    title: 'Second post',
+    description: 'Content of the second post',
+    author: 'Rose',
+  },
+];
+
+const authors = {
+  Daisy: {
+    name: 'Daisy',
+    age: 5,
+  },
+  Rose: {
+    name: 'Rose',
+    age: 2,
+  },
+};
 
 module.exports = schema;
